2d0db299410a72be99a07f6e50df81fd70a3f64e
Sverre <sverrre@stud.ntnu.no>
2020-11-10 12:05:36 +0100

    update

--- 3rd graded assignment/gradedSLAM/gradedSLAM/run_simulated_SLAM.py
+++ 3rd graded assignment/gradedSLAM/gradedSLAM/run_simulated_SLAM.py
@@ -96,12 +96,19 @@ K = len(z)
 M = len(landmarks)
 
 # %% Initilize
-Q = np.diag([1, 1, 1]) # TODO
-R = np.diag([1, 1]) # TODO
+# Q = np.diag([1, 1, 1]) # TODO
+# R = np.diag([1, 1]) # TODO
+
+Q = np.array([[(7e-2)**2,0,0],
+            [0,(7e-2)**2,0],
+            [0,0,(2e-2)**2]])*1e-1# TODO
+
+R = np.array([[(4e-2)**2, 0],
+            [0, (2e-2)**2]])*2e0# TODO
 
 doAsso = True
 
-JCBBalphas = np.array([1, 1]) # TODO,  # first is for joint compatibility, second is individual
+JCBBalphas = np.array([1e-10, 1e-10]) # TODO,  # first is for joint compatibility, second is individual
 # these can have a large effect on runtime either through the number of landmarks created
 # or by the size of the association search space.
 
@@ -152,7 +159,7 @@ for k, z_k in tqdm(enumerate(z[:N])):
 
     num_asso = np.count_nonzero(a[k] > -1)
 
-    CI[k] = chi2.interval(alpha, 2 * num_asso)
+    CI[k] = chi2.interval(1-alpha, 2 * num_asso)
 
     if num_asso > 0:
         NISnorm[k] = NIS[k] / (2 * num_asso)
